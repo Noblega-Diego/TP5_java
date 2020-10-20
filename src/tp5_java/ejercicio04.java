7728d7e563f6dde37dc246eff8539748f8fa82b5
diego <diego@laptop-swift3.com>
2020-10-20 09:40:54 -0300

    comentarios añadidos

--- src/tp5_java/ejercicio04.java
+++ src/tp5_java/ejercicio04.java
@@ -34,7 +34,12 @@ public class ejercicio04 {
             palabras.add(palabra);
         }while(true);
     }
-    
+    /**
+     * comapra las cantidades de lentras entre A y B
+     * @param A palabra1 esta se compara con la otra
+     * @param B palabra2
+     * @return etorna 1 o -1 dependiendo si palabra1 tiene mas o menos palabras
+     */
     private static int compararCantidadAB(String A, String B){
         int Alength = A.length();
         int Blength = B.length();
@@ -44,6 +49,7 @@ public class ejercicio04 {
         else return 0;
     }
     
+
     private static void mostrarPalabraConMasLetras(ArrayList<String> palabras){
         String palabraMasLetras = palabras.get(0);
         for (String palabra : palabras) {
@@ -55,27 +61,29 @@ public class ejercicio04 {
     }
     
     private static void palabrasRepetidas(ArrayList<String> palabras){
-        ArrayList<String> palabrasConcidencias = new ArrayList<String>();
+        ArrayList<String> palabrasRepetidas = new ArrayList<String>();  //donde se guardaran las palabras repetidas
         boolean coincidencia;
         for(String palabra1 : palabras){
             coincidencia = false;
-            for(String palabraCoincidencia : palabrasConcidencias){
-                if(palabra1.compareToIgnoreCase(palabraCoincidencia) == 0){
-                    coincidencia = true;
+            for(String palabraRepetida : palabrasRepetidas){            
+                if(palabra1.compareToIgnoreCase(palabraRepetida) == 0){ //si la palabra se encuentra lista de palabras repetidas
+                    coincidencia = true;                                //no es necesario que se busquen coincidencias nuevaente
                     break;
                 }
             }
-            if(!coincidencia){
+
+            if(!coincidencia){                                          
                 int cantidad;
                 cantidad = vuscarPalabra(palabras, palabra1);
-                if(cantidad > 1){
-                    palabrasConcidencias.add(palabra1);
-                    System.out.println("    "+palabra1+":"+cantidad);
+                if(cantidad > 1){                                       //si se encuentra mas de 1 ves en el ArrayList
+                    palabrasRepetidas.add(palabra1);                    //la palbra sera añadida al arrayList que contiene las palabras repetidas
+                    System.out.println("    "+palabra1+":"+cantidad);   //se imprime la palabra con la canidad de veces que esta presente en el ArrayList
                 }
             }
         }
     }
-    
+
+    //retorna la cantidad de veces que se ha encontrado la palabra en el array list
     private static int vuscarPalabra(ArrayList<String> palabras, String palabra){
         int cantidad = 0;
         for(String p : palabras){
